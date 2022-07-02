3e1fced16dd32f62a03c28e99830369e8d396354
Lester Andres Garcia Aquino <lagarcia2015url@gmail.com>
2022-07-02 17:24:14 -0600

    Adding business courses content ad creating a folder for personal content

---
+++ 02. Business content/01. Automatización de pruebas para aplicaciones y servicios web/Sesión 02/modificadores-acceso/src/protectedmodifier/package2/EjecutarProtectedDemo.java
@@ -0,0 +1,10 @@
+package protectedmodifier.package2;
+
+import protectedmodifier.package1.*;
+
+public class EjecutarProtectedDemo extends ProtectedDemo {
+    public static void maind(String[] args){
+        EjecutarProtectedDemo objEjecutarProtectedDemo = new EjecutarProtectedDemo();
+        objEjecutarProtectedDemo.mostrar();
+    }
+}
---
+++ 02. Business content/README.md
@@ -0,0 +1,6 @@
+# LaserAnts_Courses
+## LaserAnts - Learning repository 
+### Courses, learning paths, live sessions content and more...
+
+### Folder 01 -----> Automatización de pruebas para aplicaciones y servicios web
+### Folder 02 -----> ISTQB Certified foundation level - Curso de preparación
